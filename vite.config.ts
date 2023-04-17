87f1989407f64c3d0aeae921299fe3e0dacaec2f
dvkr <v3dvkr@gmail.com>
2023-04-17 15:48:37 -0600

    more redirects rules

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 let serverUrl = "";
 
-if (process.env.NODE_ENV === "production") {
-	serverUrl = "https://outstanding-plum-millipede.cyclic.app";
-} else {
-	serverUrl = "http://localhost:4000";
-}
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
 		proxy: {
-			"/api": {
-				target: serverUrl,
-				changeOrigin: true,
-			},
+			"/api": "http://localhost:4000",
 		},
 	},
 	plugins: [react()],
